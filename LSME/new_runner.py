8b278a2a957ea73a2838d1b172a7c580a593c58b
Ke Chen <kxc5915@psu.edu>
2026-08-26 19:40:11 -0400

    change to 40 epochs and repeat 4 times; add output prefix/suffix to avoid overwritting existing model files in repeated runs

--- LSME/new_runner.py
+++ LSME/new_runner.py
@@ -15,13 +15,92 @@ from model_loss_train import *
 from data_reader_pn_ import data_load_bd
 
 
-def Training_Evaluation_Parameter_Set(d1, d2, a_, path, df_tr, df_v, df_test, batch_size, delta, m_dim, num_b):
+def _expand_slurm_tokens(value):
+    """Expand supported Slurm filename tokens in a prefix or suffix."""
+    if not value:
+        return ''
+
+    token_variables = {
+        '%j': ('SLURM_JOB_ID',),
+        '%A': ('SLURM_ARRAY_JOB_ID', 'SLURM_JOB_ID'),
+        '%a': ('SLURM_ARRAY_TASK_ID',),
+    }
+    expanded = value
+    for token, variable_names in token_variables.items():
+        if token not in expanded:
+            continue
+        replacement = next(
+            (os.environ[name] for name in variable_names if os.environ.get(name)),
+            None,
+        )
+        if replacement is None:
+            variables = ' or '.join(variable_names)
+            raise ValueError(
+                f'{token} was requested, but {variables} is not set'
+            )
+        expanded = expanded.replace(token, replacement)
+
+    if os.sep in expanded or (os.altsep and os.altsep in expanded):
+        raise ValueError('output prefixes and suffixes cannot contain path separators')
+    if expanded in {'.', '..'}:
+        raise ValueError('invalid output prefix or suffix')
+    return expanded
+
+
+def _tagged_filename(stem, extension, prefix='', suffix=''):
+    """Build a filename with optional underscore-separated prefix/suffix."""
+    components = [component for component in (prefix, stem, suffix) if component]
+    return '_'.join(components) + extension
+
+
+def Training_Evaluation_Parameter_Set(d1, d2, a_, path, df_tr, df_v, df_test,
+                                      batch_size, delta, m_dim, num_b,
+                                      output_prefix='', output_suffix=''):
 
     models_path = f'{path}models/'
     os.makedirs(models_path, exist_ok=True)
 
     results_path = f'{path}results/'
     os.makedirs(results_path, exist_ok=True)
+
+    ID = f'{num_b}k_{m_dim}m_({d1}-{d2})s_{delta}delta'
+    encoder_file = os.path.join(
+        models_path,
+        _tagged_filename(
+            f'{Inp_Model_2.__name__}_{ID}', '.pt',
+            output_prefix, output_suffix,
+        ),
+    )
+    siamese_file = os.path.join(
+        models_path,
+        _tagged_filename(
+            f'{SiamNNL1.__name__}_{Inp_Model_2.__name__}_{ID}', '.pt',
+            output_prefix, output_suffix,
+        ),
+    )
+    results_file = os.path.join(
+        results_path,
+        _tagged_filename(
+            f'loss_acc_{Inp_Model_2.__name__}_{ID}', '.hdf5',
+            output_prefix, output_suffix,
+        ),
+    )
+
+    existing_files = [
+        file_path for file_path in (encoder_file, siamese_file, results_file)
+        if os.path.exists(file_path)
+    ]
+    if existing_files:
+        formatted_paths = '\n  '.join(existing_files)
+        raise FileExistsError(
+            'Refusing to overwrite existing output files:\n  '
+            f'{formatted_paths}\nChoose a different --output_prefix or --output_suffix.'
+        )
+
+    print('Output files:')
+    print(f'  encoder: {encoder_file}')
+    print(f'  model:   {siamese_file}')
+    print(f'  results: {results_file}')
 
     train_a, train_b, train_t, train_y = aby_sep(df_tr)
     valid_a, valid_b, valid_t, valid_y = aby_sep(df_v)
@@ -42,7 +121,6 @@ def Training_Evaluation_Parameter_Set(d1, d2, a_, path, df_tr, df_v, df_test, ba
     flat_dim = cnnk(a_).shape[1]
     out_dim = num_b*m_dim
     siacnn2 = SiamNNL1(cnnk, flat_dim, out_dim).to(device)
-    ID = f'{num_b}k_{m_dim}m_({d1}-{d2})s_{delta}delta'
     print(f'{ID} model construct')
     trainer1 = Trainer1(train_a, train_b, train_t, siacnn2, loss0, delta, batch_size)
     print('##########train start###########')
@@ -56,13 +134,10 @@ def Training_Evaluation_Parameter_Set(d1, d2, a_, path, df_tr, df_v, df_test, ba
         loss_t += loss1_
         loss_v += loss11_ 
 
-        torch.save(cnnk, f'{models_path}{cnnk.__class__.__name__}_{ID}.pt')
-        torch.save(siacnn2, f'{models_path}{siacnn2.__class__.__name__}_{cnnk.__class__.__name__}_{ID}.pt')
+        torch.save(cnnk, encoder_file)
+        torch.save(siacnn2, siamese_file)
         
     print('##########train end###########')
-    f = h5py.File(f'{results_path}loss_acc_{cnnk.__class__.__name__}_{ID}.hdf5', 'a')
-    f.create_dataset('loss_t', data=np.array(loss_t))
-    f.create_dataset('loss_v', data=np.array(loss_v))
 
     print('###########TESTING###############')
 
@@ -74,7 +149,6 @@ def Training_Evaluation_Parameter_Set(d1, d2, a_, path, df_tr, df_v, df_test, ba
     res_t, acc_t = breakdown_acc(eds_t, d1, d2, siacnn2, acc_count_0, batch_size, delta, m_dim, num_b, device)
     print('acc_test: '+str(acc_t))
 
-    f.create_dataset('accs', data=np.array([acc_tr, acc_t]))
     bd_res = []
     for i in sorted(res.keys()):
         bd_res.append([i, res[i]])
@@ -83,9 +157,12 @@ def Training_Evaluation_Parameter_Set(d1, d2, a_, path, df_tr, df_v, df_test, ba
     for i in sorted(res_t.keys()):
         bd_res_t.append([i, res_t[i]])
 
-    f.create_dataset('bd_train', data=np.array(np.array(bd_res)))
-    f.create_dataset('bd_test', data=np.array(np.array(bd_res_t)))
-    f.close()
+    with h5py.File(results_file, 'x') as results_handle:
+        results_handle.create_dataset('loss_t', data=np.array(loss_t))
+        results_handle.create_dataset('loss_v', data=np.array(loss_v))
+        results_handle.create_dataset('accs', data=np.array([acc_tr, acc_t]))
+        results_handle.create_dataset('bd_train', data=np.array(bd_res))
+        results_handle.create_dataset('bd_test', data=np.array(bd_res_t))
 
 
 #CUDA
@@ -106,6 +183,16 @@ def main():
                         help='Test sequence-pair file')
     parser.add_argument('--out_path', type=str, required=True,
                         help='Output directory (models/ and results/ are created inside)')
+    parser.add_argument(
+        '--output_prefix', type=str, default='',
+        help='Prefix added to every output filename. Supports Slurm tokens '
+             '%%j (job ID), %%A (array job ID), and %%a (array task ID).',
+    )
+    parser.add_argument(
+        '--output_suffix', type=str, default='',
+        help='Suffix added to every output filename. Supports Slurm tokens '
+             '%%j (job ID), %%A (array job ID), and %%a (array task ID).',
+    )
     parser.add_argument('--d1', type=int, required=True,
                         help='Largest edit distance assigned the positive label')
     parser.add_argument('--d2', type=int, required=True,
@@ -143,12 +230,19 @@ def main():
 
     out_path = os.path.abspath(os.path.expanduser(args.out_path)) + os.sep
     os.makedirs(out_path, exist_ok=True)
+    try:
+        output_prefix = _expand_slurm_tokens(args.output_prefix)
+        output_suffix = _expand_slurm_tokens(args.output_suffix)
+    except ValueError as exc:
+        parser.error(str(exc))
 
     print(f'N={args.n_len}, d1={args.d1}, d2={args.d2}, m_dim={args.m_dim}, '
           f'batch_size={args.batch_size}, num_b={args.num_b}, device={device}')
     print(f'Train files: {args.train_files}')
     print(f'Test file: {args.test_file}')
     print(f'Output path: {out_path}')
+    print(f'Output prefix: {output_prefix or "(none)"}')
+    print(f'Output suffix: {output_suffix or "(none)"}')
 
     df_tr, df_v, df_test = data_load_bd(
         args.rate, args.d1, args.d2, args.train_files, args.test_file,
@@ -158,6 +252,7 @@ def main():
     Training_Evaluation_Parameter_Set(
         args.d1, args.d2, a_, out_path, df_tr, df_v, df_test,
         args.batch_size, args.delta, args.m_dim, args.num_b,
+        output_prefix, output_suffix,
     )
     
 if __name__ == "__main__":
